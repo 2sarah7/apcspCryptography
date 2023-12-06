16f31774068b022946f449e99ba38942e1ca8e75
Sarah <awesomesarah77@icloud.com>
2023-12-06 12:58:46 -0700

    no clue

--- pwalgorithms.py
+++ pwalgorithms.py
@@ -11,12 +11,29 @@ def get_dictionary():
   return words
 
 # analyze a one-word password
-def one_word(password):
+# def one_word(password):
+#   words = get_dictionary()
+#   guesses = 0
+#   # get each word from the dictionary file
+#   for w in words:
+#     guesses += 1
+#     if (w in password):
+#       return True, guesses
+#   return False, guesses
+
+def two_words(password):
   words = get_dictionary()
+  words2 = password
   guesses = 0
-  # get each word from the dictionary file
+
   for w in words:
+    print(w)
     guesses += 1
     if (w in password):
-      return True, guesses
+      words2.replace(w, "")
+      print(words2)
+      for w in words:
+        if w in words2:
+          return True, guesses
+      
   return False, guesses
