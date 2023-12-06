c7a60b9c1646fd675e83f210e0de7cf7abb3ff26
Sa <138382677+2sarah7@users.noreply.github.com>
2023-12-06 12:19:35 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,19 @@
-#   a212_cipher.py
-#   Rotation Cipher
+# a213_pw_analyzer.py
+import time
+import pwalgorithms as pwa
 
-MAX_KEY_SIZE = 26 
+password = input("Enter password:")
 
-def getMode():
-  while True:
-    print('Do you wish to encrypt or decrypt a message?')
-    mode = input().lower()
-    if mode in 'encrypt e decrypt d brute b'.split():
-      return mode
-    else:
-      print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
+print("Analyzing a one-word password ...")
+time_start = time.time()
 
+# attempt to find password
+found, num_guesses = pwa.one_word(password)
+time_end = time.time()
 
-def getMessage():
-  print('Enter your message:')
-  return input()
-
-def getKey():
-  key = 0
-  while True:
-    print('Enter the key number (1-%s)' % (MAX_KEY_SIZE))
-    key = int(input())
-    if (key >= 1 and key <= MAX_KEY_SIZE):
-      return key
-
-def getTranslatedMessage(mode, message, key):
-  if mode[0] == 'd':
-    key = -key
-  translated = ''
-
-  for symbol in message:
-    if symbol.isalpha():
-      num = ord(symbol)
-      num += key
-
-      if symbol.isupper():
-        if num > ord('Z'):
-          num -= 26
-        elif num < ord('A'):
-          num += 26
-      elif symbol.islower():
-        if num > ord('z'):
-          num -= 26
-        elif num < ord('a'):
-          num += 26
-    
-      translated += chr(num)
-    
-    else:
-      translated += symbol
-
-  return translated
-
-mode = getMode()
-message = getMessage()
-
-if mode[0] != 'b':
-  key = getKey()
-
-
-print('Your translated text is:')
-if mode[0] != 'b':
-  print(getTranslatedMessage(mode, message, key))
-else:
-  for key in range(1, MAX_KEY_SIZE + 1):
-    print(key, getTranslatedMessage('decrypt', message, key))
+# report results
+if (found):
+  print(password, "found in", num_guesses, "guesses")
+else: 
+  print(password, "NOT found in", num_guesses, "guesses!")
+print("Time:", format((time_end-time_start), ".8f"))
