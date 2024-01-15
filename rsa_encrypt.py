109f4494b3f96187e728d4d1c4ec135fc2ce669f
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2024-01-15 10:59:55 -0700

    improved rsa encrypt

--- rsa_encrypt.py
+++ rsa_encrypt.py
@@ -1,8 +1,16 @@
 #   a212_rsa_encrypt.py
 import rsa as rsa
 
-key = int(input("Enter the Encryption Key: " ))
-mod_value = int(input("Enter the Modulus: " ))
+key = input("Enter the Encryption Key: " )
+while key.isdigit() == False:
+    key = input("Enter the Encryption key: ")
+else:
+    key = int(key)
+mod_value = input("Enter the Modulus: " )
+while mod_value.isdigit() == False:
+    mod_value = input("Enter the modulus: ")
+else:
+    mod_value = int(mod_value)
 plaintext = input("Enter a message to encrypt: ")
 encrypted_msg = rsa.encrypt(key, mod_value, plaintext)
 print("Encrypted Message:", encrypted_msg)
