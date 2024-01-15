98ad68cbdb7b985cb28b176ffe7290f6996ade6b
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2024-01-15 11:05:00 -0700

    improved rsa decrypt

--- rsa_decrypt.py
+++ rsa_decrypt.py
@@ -1,8 +1,16 @@
 #   a212_rsa_decrypt.py
 import rsa as rsa
 
-key = int(input("Enter the Decryption Key: " ))
-mod_value = int(input("Enter the Modulus: " ))
+key = input("Enter the Decryption Key: " )
+while key.isdigit() == False:
+    key = input("Enter the Decryption key: ")
+else:
+    key = int(key)
+mod_value = input("Enter the Modulus: " )
+while mod_value.isdigit() == False:
+    mod_value = input("Enter the modulus: ")
+else:
+    mod_value = int(mod_value)
 encrypted_msg = input("What message would you like to decrypt (No brackets): ")
 
 #break apart the list that is cut/copied over on ", "
